796cd625d8264d4b2f58f0257b29d95097bad962
yang rouman <rouman321@163.com>
2018-04-07 23:59:52 +0800

    providing a paticular item_id you can now get the info using getItemInfo, get the questions and answers from sellers using getSellerQA. Also new questions and its answer can be updated into the database by updateQA

--- src/back_end/app/models.py
+++ src/back_end/app/models.py
@@ -3,13 +3,46 @@ import jieba
 from gensim.models import Word2Vec
 from pprint import pprint
 
-client = pymongo.MongoClient('localhost',27017)
-db = client["JD"]
-collection = db["item_info"]
-
 item_id = 5706771
 
-info = collection.find({'item_id': item_id})[0]["item_info"]
+#info = collection.find({'item_id': item_id})[0]["item_info"]
 
-model = Word2Vec.load("app/text_processor/wordvec.model")
+#model = Word2Vec.load("app/text_processor/wordvec.model")
 
+#get the information of a particular item whose id is id
+def getItemInfo(id):
+    client = pymongo.MongoClient('localhost',27017)
+    db = client["JD"]
+    set = db["item_info"]
+    info = set.find({'item_id':id})[0]
+    client.close()
+    return info
+
+#get those questions and corresponding answers of a particular item whose id is id
+def getSellerQA(id):
+    client = pymongo.MongoClient('localhost', 27017)
+    db = client["JD"]
+    set = db["seller_qa"]
+    ret = set.find({'item_id':id})[0]['seller_qa']
+    client.close()
+    return ret
+
+def updateQA(id,question,answer):
+    qaDict = {}
+    qaDict['question'] = question
+    qaDict['answer'] = answer
+    client = pymongo.MongoClient('localhost', 27017)
+    db = client["JD"]
+    set = db["seller_qa"]
+    oldDict = []
+    oldDict = set.find({'item_id':id})[0]['seller_qa']
+    #print(type(oldDict))
+    oldDict.append(qaDict)
+    #print(oldDict)
+    set.update({'item_id':id},{'$set':{'seller_qa':oldDict}})
+    client.close()
+
+if __name__ == '__main__':
+    updateQA(4586850,"试试进去了么","进去了")
+    info = getSellerQA(4586850)
+    print(info)
